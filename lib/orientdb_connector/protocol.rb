0ef22b2e2a719a54d5bac990f7c09ce6f058c82d
Pawel Barcik <pawel.barcik@gmail.com>
2012-07-28 20:53:21 +0100

    remove operation types

--- lib/orientdb_connector/protocol.rb
+++ lib/orientdb_connector/protocol.rb
@@ -4,7 +4,5 @@ module OrientDBConnector
   end
 end
 
-require "orientdb_connector/protocol/operation_types"
 require "orientdb_connector/protocol/response_statuses"
-
 require "orientdb_connector/protocol/commands"
