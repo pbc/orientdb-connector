9b7fb3ec1b8690772e2b2fcc9b3319b9afe5618d
Pawel Barcik <pawel.barcik@gmail.com>
2012-11-16 14:40:57 +0000

    add more conversion functionality to the OrientDBConnector::Protocol::Record

--- lib/orientdb_connector/protocol/record.rb
+++ lib/orientdb_connector/protocol/record.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'bigdecimal'
 
 module OrientDBConnector
   module Protocol
@@ -28,21 +29,36 @@ module OrientDBConnector
       end
 
       def serialize_number(number)
-        number.to_s +
+        number_to_string(number) + number_type_code(number)
+      end
+
+      def number_to_string(number)
+        case number
+        when Float
+          number.to_s
+        when BigDecimal
+          number.to_s("F")
+        when Integer
+          number.to_s
+        end
       end
 
       def number_type_code(number)
         case number.class
         when Float
           "f"
-        when Integer && number < 256
+        when Integer && -128 <= number && number <= 127
+          "b"
+        when Integer && -32768 <= number && number <= 32767
+          "s"
+        when Integer && -2147483648 <= number && number <= 2147483647
           ""
-        when Integer && number < 256 ^ 2
-          ""
-        when Integer && number < 256 ^ 4
-          ""
-        when Integer && number < 256 ^ 8
-          ""
+        when Integer && -9223372036854775808 <= number && number <= 9223372036854775808
+          "l"
+        when Integer && (-9223372036854775808 > number || number > 9223372036854775808)
+          "d"
+        when BigDecimal
+          "c"
         end
       end
 
