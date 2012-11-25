6b3df34470a92c9ab9aca6d87d33829b4c2e7274
Pawel Barcik <pawel.barcik@gmail.com>
2012-11-25 15:32:05 +0000

    add more OrientDBConnector::Protocol::Number specs

--- spec/lib/orientdb_connector/protocol/number_spec.rb
+++ spec/lib/orientdb_connector/protocol/number_spec.rb
@@ -1,13 +1,12 @@
 require "spec_helper"
 
-describe OrientDBConnector::Protocol::Record do
+describe OrientDBConnector::Protocol::Number do
 
   let(:number_class) {OrientDBConnector::Protocol::Number}
 
   context "#to_s" do
     context "when current value is an Integer >= 0" do
       it "should return correct string" do
-        #raise number_class.new(12342387456923874659823476539824756928347562938476582934765).to_s
         number_class.new(12342387456923874659823476539824756928347562938476582934765).to_s.should == "12342387456923874659823476539824756928347562938476582934765"
       end
     end
@@ -70,25 +69,85 @@ describe OrientDBConnector::Protocol::Record do
         end
       end
 
-
-
       context "number == -32768" do
-        it "should return 'b'" do
-          number_class.new(-128).type_code.should == "b"
+        it "should return 's'" do
+          number_class.new(-32768).type_code.should == "s"
         end
       end
 
-      context "-128 < number < 127" do
-        it "should return 'b'" do
-          number_class.new(12).type_code.should == "b"
+      context "-32768 < number < -128" do
+        it "should return 's'" do
+          number_class.new(-129).type_code.should == "s"
+          number_class.new(-32767).type_code.should == "s"
         end
       end
 
-      context "number == 127" do
-        it "should return 'b'" do
-          number_class.new(127).type_code.should == "b"
+      context "127 < number < 32767" do
+        it "should return 's'" do
+          number_class.new(128).type_code.should == "s"
+          number_class.new(32766).type_code.should == "s"
         end
       end
+
+      context "number == 32767" do
+        it "should return 's'" do
+          number_class.new(32767).type_code.should == "s"
+        end
+      end
+
+      context "number == -2147483648" do
+        it "should return empty string" do
+          number_class.new(-2147483648).type_code.should == ""
+        end
+      end
+
+      context "-2147483648 < number < -32768" do
+        it "should return empty string" do
+          number_class.new(-32769).type_code.should == ""
+          number_class.new(-2147483647).type_code.should == ""
+        end
+      end
+
+      context "32767 < number < 2147483647" do
+        it "should return empty string" do
+          number_class.new(32768).type_code.should == ""
+          number_class.new(2147483646).type_code.should == ""
+        end
+      end
+
+      context "number == 2147483647" do
+        it "should return empty string" do
+          number_class.new(2147483647).type_code.should == ""
+        end
+      end
+
+      context "number == -9223372036854775808" do
+        it "should return 'l'" do
+          number_class.new(-9223372036854775808).type_code.should == "l"
+        end
+      end
+
+      context "-9223372036854775808 < number < -2147483648" do
+        it "should return 'l'" do
+          number_class.new(-2147483649).type_code.should == "l"
+          number_class.new(-9223372036854775807).type_code.should == "l"
+        end
+      end
+
+      context "2147483647 < number < 9223372036854775807" do
+        it "should return 'l'" do
+          number_class.new(2147483648).type_code.should == "l"
+          number_class.new(9223372036854775806).type_code.should == "l"
+        end
+      end
+
+      context "number == 9223372036854775807" do
+        it "should return 'l'" do
+          number_class.new(9223372036854775807).type_code.should == "l"
+        end
+      end
+
+
 
     end
   end
