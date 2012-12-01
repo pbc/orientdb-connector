ccc0fc4b44232e500fa6ea0adcf8cdb52874f345
Pawel Barcik <pawel.barcik@gmail.com>
2012-12-01 20:27:32 +0000

    add serialization methods for booleans and time related objects

--- lib/orientdb_connector/protocol/record.rb
+++ lib/orientdb_connector/protocol/record.rb
@@ -3,6 +3,7 @@ require 'base64'
 module OrientDBConnector
   module Protocol
     class Record
+
       def serialize
 
       end
@@ -17,8 +18,8 @@ module OrientDBConnector
         end
       end
 
-      def deserialize_string(content_string)
-        content_string.gsub(/(\\(\\|"))/) do |match|
+      def deserialize_string(encoded_content_string)
+        encoded_content_string.gsub(/(\\(\\|"))/) do |match|
           match[1..-1]
         end
       end
@@ -31,8 +32,8 @@ module OrientDBConnector
         number.serialize
       end
 
-      def deserialize_number(number)
-        number.deserialize
+      def deserialize_number(encoded_number)
+        encoded_number.deserialize
       end
 
       def encode_binary_content(binary_content)
@@ -41,6 +42,39 @@ module OrientDBConnector
 
       def decode_binary_content(encoded_content)
         Base64.decode64(encoded_content.gsub("_", "")).to_s
+      end
+
+      def serialize_boolean(boolean_value)
+        boolean_value.to_s
+      end
+
+      def deserialize_boolean(encoded_value)
+        return true if encoded_value === "true"
+        return false if encoded_value === "false"
+        raise StandardError.new("provided encoded value is invalid for a Boolean")
+      end
+
+      def is_encoded_boolean?(value)
+        value.match(/\A(true|false)\z/) != nil
+      end
+
+      def serialize_datetime(value)
+        return (value.to_time.to_f * 1000).to_i.to_s + "t" if value.class == DateTime
+        return (value.to_f * 1000).to_i.to_s  + "t" if value.class == Time
+        raise StandardError.new("provided value can't be serialized by this method")
+      end
+
+      def deserialize_datetime(encoded_value)
+
+      end
+
+      def serialize_date(value)
+        return (value.to_time.to_f * 1000).to_i.to_s + "a" if value.class == Date
+        raise StandardError.new("provided value can't be serialized by this method")
+      end
+
+      def deserialize_date(encoded_value)
+
       end
 
     end
--- spec/lib/orientdb_connector/protocol/record_spec.rb
+++ spec/lib/orientdb_connector/protocol/record_spec.rb
@@ -11,6 +11,12 @@ describe OrientDBConnector::Protocol::Record do
     end
   end
 
+  context "#deserialize" do
+    it "should deserialize " do
+      pending
+    end
+  end
+
   context "#serialize_string" do
     it "should escape properly double quote characters" do
       record.serialize_string('"').should == '\"'
@@ -72,5 +78,118 @@ describe OrientDBConnector::Protocol::Record do
     end
   end
 
+  context "#serialize_boolean" do
+    context "when the provided value is true" do
+      it "should return string 'true'" do
+        record.serialize_boolean(true).should == "true"
+      end
+    end
+
+    context "when the provided value is false" do
+      it "should return string 'false'" do
+        record.serialize_boolean(false).should == "false"
+      end
+    end
+  end
+
+  context "#deserialize_boolean" do
+    context "when the provided value is a string 'true'" do
+      it "should return boolean true" do
+        record.deserialize_boolean("true").should === true
+      end
+    end
+
+    context "when the provided value is a string 'false'" do
+      it "should return boolean false" do
+        record.deserialize_boolean("false").should === false
+      end
+    end
+
+    context "provided string is not 'true' or 'false'" do
+      it "should raise an error" do
+        error_message = "provided encoded value is invalid for a Boolean"
+        lambda {
+          record.deserialize_boolean(" false ")
+        }.should raise_error(StandardError, error_message)
+
+        lambda {
+          record.deserialize_boolean(" true ")
+        }.should raise_error(StandardError, error_message)
+
+        lambda {
+          record.deserialize_boolean(" dskjhf jsdhfg ajsd ").should === false
+        }.should raise_error(StandardError, error_message)
+      end
+    end
+  end
+
+  context "#is_encoded_boolean?" do
+
+    context "provided string is not 'true' or 'false'" do
+      it "should be false" do
+        record.is_encoded_boolean?(" true ").should === false
+        record.is_encoded_boolean?(" false ").should === false
+        record.is_encoded_boolean?(" foobar ").should === false
+        record.is_encoded_boolean?("foobar").should === false
+      end
+    end
+
+    context "provided string is 'true'" do
+      it "should be true" do
+        record.is_encoded_boolean?("true").should === true
+      end
+    end
+
+    context "provided string is 'false'" do
+      it "should be false" do
+        record.is_encoded_boolean?("false").should === true
+      end
+    end
+  end
+
+  context "#serialize_datetime" do
+
+    context "provided value is a DateTime object" do
+      it "should return correctly formatted string" do
+        date_time = DateTime.parse("2012-12-12 12:23:44.235")
+        record.serialize_datetime(date_time).should == "1355315024235t"
+      end
+    end
+
+    context "provided value is a Time object" do
+      it "should return correctly formatted string" do
+        date_time = Time.at(1354393072.640)
+        record.serialize_datetime(date_time).should == "1354393072640t"
+      end
+    end
+
+    context "provided value is not a Time or DateTime object" do
+      it "should raise an error" do
+        date_time = 1354393072.640
+        lambda {
+          record.serialize_datetime(date_time)
+        }.should raise_error(StandardError, "provided value can't be serialized by this method")
+      end
+    end
+
+  end
+
+  context "#serialize_date" do
+    context "provided value is a Date object" do
+      it "should return correctly formatted string" do
+        date_time = Date.parse("2012-12-12 00:00:00")
+        record.serialize_date(date_time).should == "1355270400000a"
+      end
+    end
+
+    context "provided value is not a Date object" do
+      it "should raise an error" do
+        date_time = 1354393072.640
+        lambda {
+          record.serialize_datetime(date_time)
+        }.should raise_error(StandardError, "provided value can't be serialized by this method")
+      end
+    end
+  end
 
 end
